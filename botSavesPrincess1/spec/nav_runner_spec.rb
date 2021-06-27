3468517f64ba14457d6e612551314965693b293f
Aiden Mendez <aidenmendez@gmail.com>
2021-06-27 10:14:50 -0600

    Remove grid from Navigation class params in tests

--- botSavesPrincess1/spec/nav_runner_spec.rb
+++ botSavesPrincess1/spec/nav_runner_spec.rb
@@ -6,15 +6,19 @@ require './lib/navigation'
 require './lib/nav_runner'
 
 RSpec.describe NavRunner do
-  describe 'happy path' do
+  describe '(happy path)' do
     let(:grid) {Grid.new(3, ["p--", "-m-", "---"])}
     let(:bot) {Bot.new(grid)}
     let(:princess) {Princess.new(grid)}
-    let(:navigation) {Navigation.new(grid, princess, bot)}
+    let(:navigation) {Navigation.new(princess, bot)}
     let(:nav_runner) {NavRunner.new(navigation)}
 
     it "is an instance of NavRunner" do
       expect(nav_runner).to be_a(NavRunner)
+    end
+
+    it "outputs directions to the console" do
+      expect(nav_runner.save_princess).to output("UP \n LEFT")
     end
   end
 end
--- botSavesPrincess1/spec/navigation_spec.rb
+++ botSavesPrincess1/spec/navigation_spec.rb
@@ -9,14 +9,13 @@ RSpec.describe Navigation do
     let(:grid) {Grid.new(3, ["p--", "-m-", "---"])}
     let(:bot) {Bot.new(grid)}
     let(:princess) {Princess.new(grid)}
-    let(:navigation) {Navigation.new(grid, princess, bot)}
+    let(:navigation) {Navigation.new(princess, bot)}
 
     it "is an instance of Navigation" do
       expect(navigation).to be_a(Navigation)
     end
 
     it "has correct attributes" do
-      expect(navigation.grid).to be_a(Grid)
       expect(navigation.princess).to be_a(Princess)
       expect(navigation.bot).to be_a(Bot)
     end
