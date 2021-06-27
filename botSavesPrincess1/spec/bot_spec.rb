3f1eea7912bf191767b554a87f92f011ca19cb51
Aiden Mendez <aidenmendez@gmail.com>
2021-06-27 11:53:46 -0600

    Test bot class for scalability

--- botSavesPrincess1/spec/bot_spec.rb
+++ botSavesPrincess1/spec/bot_spec.rb
@@ -4,16 +4,32 @@ require './lib/bot'
 
 RSpec.describe Bot do
   describe "(happy path)" do
-    let(:grid) {Grid.new(3, ["p--", "-m-", "---"])}
-    let(:bot) {Bot.new(grid)}
-
-    it "is an instance of Bot" do
-      expect(bot).to be_a(Bot)
+    describe "(size 3)" do
+      let(:grid) {Grid.new(3, ["p--", "-m-", "---"])}
+      let(:bot) {Bot.new(grid)}
+  
+      it "is an instance of Bot" do
+        expect(bot).to be_a(Bot)
+      end
+  
+      it "has x and y coordinates" do
+        expect(bot.x).to eq(1)
+        expect(bot.y).to eq(1)
+      end
     end
 
-    it "has x and y coordinates" do
-      expect(bot.x).to eq(1)
-      expect(bot.y).to eq(1)
+    describe "(size 5)" do
+      let(:grid) {Grid.new(5, ["----p", "-----", "--m--", "-----", "-----"])}
+      let(:bot) {Bot.new(grid)}
+  
+      it "is an instance of Bot" do
+        expect(bot).to be_a(Bot)
+      end
+  
+      it "has x and y coordinates" do
+        expect(bot.x).to eq(2)
+        expect(bot.y).to eq(2)
+      end
     end
   end
 end
