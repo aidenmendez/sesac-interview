bc6d9eb72f587cadc54d0de325df8197268c0b28
Aiden Mendez <aidenmendez@gmail.com>
2021-06-26 20:40:45 -0600

    Refactor grid tests and test for grid scalability

--- botSavesPrincess1/spec/grid_spec.rb
+++ botSavesPrincess1/spec/grid_spec.rb
@@ -3,26 +3,23 @@ require './lib/grid'
 
 RSpec.describe Grid do
   describe "(happy path)" do
-    size = 3
-    let(:grid) {Grid.new(size)}
+    let(:grid) {Grid.new(3, ["p--", "-m-", "---"])}
 
     it "is an instance of Grid" do
       expect(grid).to be_a(Grid)
     end
 
     it "has valid attributes" do
-      expect(grid.size).to eq(size)
-      expect(grid.layout).to eq([['p--'], ['-m-'], ['---']])
-      expect(grid.my_location).to eq([2,2])
+      expect(grid.size).to eq(3)
+      expect(grid.bot_location).to eq([1,1])
       expect(grid.princess_location).to eq([0,0])
     end
 
     it "can have dimensions greater than three" do
-      grid2 = Grid.new(9)
-      expect(grid.size).to eq(size)
-      expect(grid.layout).to eq([['p--------'], ['----m----'], ['---------']])
-      expect(grid.my_location).to eq([5,5])
-      expect(grid.princess_location).to eq([0,0])
+      grid2 = Grid.new(5, ["----p", "-----", "--m--", "-----", "-----"])
+      expect(grid2.size).to eq(5)
+      expect(grid2.bot_location).to eq([2,2])
+      expect(grid2.princess_location).to eq([0,4])
     end
   end
 end
