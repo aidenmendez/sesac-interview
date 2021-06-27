a1ba95368c644136f8a1095ee437316fbc66fbe6
Aiden Mendez <aidenmendez@gmail.com>
2021-06-27 10:27:44 -0600

    Test runner file instantiates objects properly

--- botSavesPrincess1/spec/nav_runner_spec.rb
+++ botSavesPrincess1/spec/nav_runner_spec.rb
@@ -7,18 +7,21 @@ require './lib/nav_runner'
 
 RSpec.describe NavRunner do
   describe '(happy path)' do
-    let(:grid) {Grid.new(3, ["p--", "-m-", "---"])}
-    let(:bot) {Bot.new(grid)}
-    let(:princess) {Princess.new(grid)}
-    let(:navigation) {Navigation.new(princess, bot)}
-    let(:nav_runner) {NavRunner.new(navigation)}
+    # let(:grid) {Grid.new(3, ["p--", "-m-", "---"])}
+    # let(:bot) {Bot.new(grid)}
+    # let(:princess) {Princess.new(grid)}
+    # let(:navigation) {Navigation.new(princess, bot)}
+    let(:nav_runner) {NavRunner.new(3, ["p--", "-m-", "---"])}
 
     it "is an instance of NavRunner" do
       expect(nav_runner).to be_a(NavRunner)
     end
 
-    it "outputs directions to the console" do
-      expect(nav_runner.save_princess).to output("UP \n LEFT")
+    it "has correct attributes" do
+      expect(nav_runner.grid).to be_a(Grid)
+      expect(nav_runner.bot).to be_a(Bot)
+      expect(nav_runner.princess).to be_a(Princess)
+      expect(nav_runner.navigation).to be_a(Navigation)
     end
   end
 end
