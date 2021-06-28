4d749f5ceb89b09037d22b06899405373cb9171e
Aiden Mendez <aidenmendez@gmail.com>
2021-06-27 21:11:11 -0600

    Update locate princess method to search for princess

--- botSavesPrincess2/lib/princess.rb
+++ botSavesPrincess2/lib/princess.rb
@@ -3,22 +3,18 @@ class Princess
 
   def initialize(grid)
     @grid = grid
-    @row = self.locate[0]
-    @col = self.locate[1]
+    @row = nil
+    @col = nil
+    self.locate
   end
   
   def locate
-    limit = @grid.size - 1
-    possible_locations = [[0, 0], [0, limit], [limit, 0], [limit, limit]]
-    
-    location = possible_locations.find do |coord|
-      @grid.input_grid[coord[0]][coord[1]] == 'p'
-    end
-
-    if location == nil
-      raise ArgumentError.new('Princess is not in a corner or does not exist')
-    else
-      location
+    @grid.input_grid.each_with_index do |row, i|
+      if row.include?('p')
+        @row = i
+        @col = row.index('p')
+        break
+      end
     end
   end
 end
