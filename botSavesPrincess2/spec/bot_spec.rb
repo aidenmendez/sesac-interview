a2808c6b3003088b42dc59b544e06cf5d5262125
Aiden Mendez <aidenmendez@gmail.com>
2021-06-27 20:50:51 -0600

    Update bot tests for new row and column params

--- botSavesPrincess2/spec/bot_spec.rb
+++ botSavesPrincess2/spec/bot_spec.rb
@@ -4,32 +4,15 @@ require './lib/bot'
 
 RSpec.describe Bot do
   describe "(happy path)" do
-    describe "(size 3)" do
-      let(:grid) {Grid.new(3, ["p--", "-m-", "---"])}
-      let(:bot) {Bot.new(grid)}
+    let(:bot) {Bot.new(2, 4)}
   
-      it "is an instance of Bot" do
-        expect(bot).to be_a(Bot)
-      end
-  
-      it "has x and y coordinates" do
-        expect(bot.x).to eq(1)
-        expect(bot.y).to eq(1)
-      end
+    it "is an instance of Bot" do
+      expect(bot).to be_a(Bot)
     end
 
-    describe "(size 5)" do
-      let(:grid) {Grid.new(5, ["----p", "-----", "--m--", "-----", "-----"])}
-      let(:bot) {Bot.new(grid)}
-  
-      it "is an instance of Bot" do
-        expect(bot).to be_a(Bot)
-      end
-  
-      it "has x and y coordinates" do
-        expect(bot.x).to eq(2)
-        expect(bot.y).to eq(2)
-      end
+    it "has x and y coordinates" do
+      expect(bot.row).to eq(2)
+      expect(bot.col).to eq(4)
     end
   end
 end
