7747fc7fb577ee7a41ae9892dcb719a381968d8c
Aiden Mendez <aidenmendez@gmail.com>
2021-06-27 19:50:10 -0600

    Set up tests/ classes from BSP1 and create gemfile

---
+++ botSavesPrincess2/spec/grid_spec.rb
@@ -0,0 +1,40 @@
+require 'rspec'
+require './lib/grid'
+
+RSpec.describe Grid do
+  describe "(happy path)" do
+    let(:grid) {Grid.new(3, ["p--", "-m-", "---"])}
+
+    it "is an instance of Grid" do
+      expect(grid).to be_a(Grid)
+    end
+
+    it "has valid attributes" do
+      expect(grid.size).to eq(3)
+      expect(grid.bot_location).to eq([1,1])
+    end
+
+    it "can have dimensions greater than three" do
+      grid2 = Grid.new(5, ["----p", "-----", "--m--", "-----", "-----"])
+      expect(grid2.size).to eq(5)
+      expect(grid2.bot_location).to eq([2,2])
+      # expect(grid2.princess_location).to eq([0,4])
+    end
+  end
+
+  describe "(sad path)" do
+    it "raises error if size param is less than 3" do
+      expect{ Grid.new(2, ["p-", "-m"]) }.to raise_error(ArgumentError)    
+    end
+
+    it "raises an error if size is an even number" do
+      expect{ Grid.new(4, ["p---", "-m--", "----", "----"])}.to raise_error(ArgumentError)
+    end
+
+    it "raises an error if size and actual grid size don't match" do
+      expect{ Grid.new(3, ["p--", "-m-"]) }.to raise_error(ArgumentError)
+      expect{ Grid.new(3, ["p-", "-m-", "---"]) }.to raise_error(ArgumentError)
+      expect{ Grid.new(3, ["p-", "-m---", "---"]) }.to raise_error(ArgumentError)
+    end
+  end
+end
---
+++ botSavesPrincess2/spec/princess_spec.rb
@@ -0,0 +1,58 @@
+require 'rspec'
+require './lib/grid'
+require './lib/princess'
+
+RSpec.describe Princess do
+  describe "(happy path)" do
+    describe "(size 3, top-left)" do
+      let(:grid) {Grid.new(3, ["p--", "-m-", "---"])}
+      let(:princess) {Princess.new(grid)}
+  
+      it "is an instance of Princess" do
+        expect(princess).to be_a(Princess)
+      end
+  
+      it "has x and y coordinates" do
+        expect(princess.row).to eq(0)
+        expect(princess.col).to eq(0)
+      end
+    end
+
+    describe "(size 3, bottom-left)" do
+      let(:grid) {Grid.new(3, ["---", "-m-", "p--"])}
+      let(:princess) {Princess.new(grid)}
+  
+      it "is an instance of Princess" do
+        expect(princess).to be_a(Princess)
+      end
+  
+      it "has x and y coordinates" do
+        expect(princess.row).to eq(2)
+        expect(princess.col).to eq(0)
+      end
+    end
+
+    describe "(size 5, top-right)" do
+      let(:grid) {Grid.new(5, ["----p", "-----", "--m--", "-----", "-----"])}
+      let(:princess) {Princess.new(grid)}
+  
+      it "is an instance of Princess" do
+        expect(princess).to be_a(Princess)
+      end
+  
+      it "has x and y coordinates" do
+        expect(princess.row).to eq(0)
+        expect(princess.col).to eq(4)
+      end
+    end
+  end
+
+  describe "(sad path)" do
+    let(:grid) {Grid.new(5, ["-----", "-----", "--m--", "-----", "-----"])}
+    let(:princess) {Princess.new(grid)}
+
+    it "raises an error if princess can't be found" do
+      expect{ Princess.new(grid) }.to raise_error(ArgumentError)
+    end
+  end
+end
